d6bf7268032f3f878d1518ed9773bbdc7b34b9e2
leon0113 <tahjib.leon399@gmail.com>
2023-02-13 23:52:49 +0600

    first commit

---
+++ src/components/Restaurant/Restaurant.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Restaurant = () => {
+    const searchFood = e => {
+        console.log(e.target.value);
+    }
+    return (
+        <div>
+            <h2>Find the food you want</h2>
+            <input onChange={searchFood} type="text" />
+        </div>
+    );
+};
+
+export default Restaurant;
