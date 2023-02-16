6fc1f900aa42e75171d758db234c04bc909fa58d
leon0113 <tahjib.leon399@gmail.com>
2023-02-16 22:42:33 +0600

    calling searched data

--- src/components/Restaurant/Restaurant.js
+++ src/components/Restaurant/Restaurant.js
@@ -1,13 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Restaurant = () => {
+    const [searchText, setSearchText] = useState('');
+    const [meals, setMeals] = useState([]);
+
+    useEffect(() => {
+        const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchText}`;
+        fetch(url)
+            .then(res => res.json())
+            .then(data => setMeals(data.meals))
+    }, [searchText]);
+
     const searchFood = e => {
-        console.log(e.target.value);
+        setSearchText(e.target.value);
     }
     return (
         <div>
             <h2>Find the food you want</h2>
-            <input onChange={searchFood} type="text" />
+            <input onChange={searchFood} type="text" /> <br />
+            <h3>Result found : {meals.length}</h3>
+            <div>
+                {
+                    meals.map(meal =>)
+                }
+            </div>
         </div>
     );
 };
